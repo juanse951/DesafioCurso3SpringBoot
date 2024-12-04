e612ada384fad23bcf4869bfbd93a9d7960f986c
juanse951 <juanse951@gmail.com>
2024-12-04 08:46:10 -0500

    se agregaron 4 dependencias: Spring Web/PostgresSQL Driver/Spring Boot DevTools/Spring Data JPA

--- src/main/java/com/aluracursos/desafio3frases/Desafio3frasesApplication.java
+++ src/main/java/com/aluracursos/desafio3frases/Desafio3frasesApplication.java
@@ -3,6 +3,7 @@ package com.aluracursos.desafio3frases;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Desafio3frasesApplication {
 
