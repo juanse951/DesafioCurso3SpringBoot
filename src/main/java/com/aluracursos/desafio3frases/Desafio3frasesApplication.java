2a8fca6d86d6bb434ab53c41739f1b2267fa9ef2
juanse951 <juanse951@gmail.com>
2024-12-04 08:43:44 -0500

    Creando la base spring initializr, se descarga el front vsc

---
+++ src/main/java/com/aluracursos/desafio3frases/Desafio3frasesApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.desafio3frases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio3frasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio3frasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/desafio3frases/Desafio3frasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.desafio3frases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio3frasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
