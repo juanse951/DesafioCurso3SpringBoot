767ee6c6f4fe551376e519a77e070e92b5d9e42b
juanse951 <juanse951@gmail.com>
2024-12-04 10:03:14 -0500

    creamos en FraseService una intancia de tipo Frase con variable frase donde se guardara toda la informacion que llegue del repositorio, retornamos un new FRasesDTO donde vamos a convertir la informacion de frase a frase DTO

--- src/main/java/com/aluracursos/desafio3frases/service/FrasesService.java
+++ src/main/java/com/aluracursos/desafio3frases/service/FrasesService.java
@@ -1,6 +1,7 @@
 package com.aluracursos.desafio3frases.service;
 
 import com.aluracursos.desafio3frases.dto.FraseDTO;
+import com.aluracursos.desafio3frases.model.Frase;
 import com.aluracursos.desafio3frases.repository.FraseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ public class FrasesService {
     private FraseRepository repository;
 
     public FraseDTO obtenerFraseAleatoria() {
-        return repository.obtenerFraseAleatoria();
+        Frase frase =  repository.obtenerFraseAleatoria();
+        return new FraseDTO(frase.getTitulo(),frase.getFrase(),frase.getPersonaje(),frase.getPoster());
     }
 }
